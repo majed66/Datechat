bd30de12d58cea728fbadc38ce1d0b113fbaa07c
majed66 <majedshafei95@gmail.com>
2025-06-28 08:04:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! من ربات DateChat هستم 😊")
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("ربات DateChat با موفقیت اجرا شد.")
+    app.run_polling()
